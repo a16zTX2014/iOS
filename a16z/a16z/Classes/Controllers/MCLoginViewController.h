c3918dd0c755484d2d7ba0ff362d3dc6c47acad4
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-06-21 15:56:01 -0700

    LoginViewController yo, project structure changed

---
+++ a16z/a16z/Classes/Controllers/MCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCLoginViewController.h
+//  a16z
+//
+//  Created by Matthew Ebeweber on 6/21/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCLoginViewController : UIViewController
+
+@end
