fd59eec7cf3cbb637606ac00f24144516cf624b3
Comyar Zaheri <comyarzaheri@utexas.edu>
2014-06-21 15:37:29 -0700

    init commit

---
+++ a16z/a16z/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  a16z
+//
+//  Created by Comyar Zaheri on 6/21/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ a16z/a16z/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  a16z
+//
+//  Created by Comyar Zaheri on 6/21/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
