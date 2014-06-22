c3a6c390c656328f0ca4ae6b9c7177784f1a0fd7
Comyar Zaheri <comyarzaheri@utexas.edu>
2014-06-21 20:04:10 -0700

    did more codes yo

---
+++ a16z/a16z/Classes/Models/MCProfileManager.m
@@ -0,0 +1,83 @@
+//
+//  MCProfileManager.m
+//  a16z
+//
+//  Created by Comyar Zaheri on 6/21/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import "MCProfileManager.h"
+
+@implementation MCProfileManager
+
+- (instancetype)_init
+{
+    if (self = [super init]) {
+        [self initializeSkills];
+        _name = [[NSUserDefaults standardUserDefaults]objectForKey:@"name"];
+        _image = [[NSUserDefaults standardUserDefaults]objectForKey:@"image"];
+        _school = [[NSUserDefaults standardUserDefaults]objectForKey:@"school"];
+    }
+    return self;
+}
+
+- (void)initializeSkills
+{
+    NSData *skillsData = [[NSUserDefaults standardUserDefaults]objectForKey:@"skills"];
+    if (skillsData) {
+        _skills = [NSKeyedUnarchiver unarchiveObjectWithData:skillsData];
+    }
+    if (!_skills) {
+        NSMutableDictionary *skills = [NSMutableDictionary new];
+        NSArray *skillList = @[@"iOS"];
+        for (NSString *skill in skillList) {
+            skills[skill] = @(NO);
+        }
+    }
+}
+
+- (instancetype)init
+{
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:@"Cannot initialize singleton"
+                                 userInfo:nil];
+}
+
++ (MCProfileManager *)sharedManager
+{
+    static dispatch_once_t onceToken;
+    static MCProfileManager *sharedManager = nil;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[MCProfileManager alloc]_init];
+    });
+    return sharedManager;
+}
+
+- (void)setName:(NSString *)name
+{
+    _name = name;
+    [[NSUserDefaults standardUserDefaults]setObject:name forKey:@"name"];
+}
+
+- (void)setImage:(UIImage *)image
+{
+    _image = image;
+    [[NSUserDefaults standardUserDefaults]setObject:image forKey:@"image"];
+}
+
+- (void)setSchool:(NSString *)school
+{
+    _school = school;
+    [[NSUserDefaults standardUserDefaults]setObject:school forKey:@"school"];
+}
+
+- (void)setSkills:(NSDictionary *)skills
+{
+    if (skills) {
+        _skills = skills;
+        NSData *data = [NSKeyedArchiver archivedDataWithRootObject:skills];
+        [[NSUserDefaults standardUserDefaults]setObject:data forKey:@"skills"];
+    }
+}
+
+@end
